cd45740e7161fe8aa6eb419a650fbbe4903cf394
Nick DiRienzo <nick@dirienzo.co>
2013-09-07 21:00:19 -0400

    Six

--- rdio.go
+++ rdio.go
@@ -136,7 +136,7 @@ func (self *Api) SearchRdio(w *rest.ResponseWriter, r *rest.Request) {
 	q := r.URL.Query().Get("q")
 	rdio := authedRdioClient(at.Value, ats.Value)
 	options := make(map[string]string)
-	options["count"] = "5"
+	options["count"] = "6"
 	ret, err := rdio.Search(q, "Artist,Album,Track", options)
 	if err != nil {
 		log.Println("Rdio Search Fail:", err.Error())
